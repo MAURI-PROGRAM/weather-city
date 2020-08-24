42bca1d0dd1ce1e23015ae6c3d550daabb97ea5b
Michael Merchan <michael.merchan@iroute.com.ec>
2020-08-24 18:49:31 -0500

    Se obtinen los valores requeridos

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -8,16 +8,38 @@ import { IWeatherApiResponse } from '../Interface/iweather-api-response';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements OnInit {
+  datosClima: IWeatherApiResponse;
+  ciudad:string;
+  pais:string;
+  temperatura:number;
+  velocidadViento : number;
+  humedad:number;
+  climaActual:string;
 
   constructor(
     private _weatherApiService: WeatherApiService
   ) {
-      _weatherApiService.getWeather().subscribe((res:IWeatherApiResponse) => {
-        console.log(res)
-      });
+      
    }
 
   ngOnInit(): void {
+    this.cargarClima();
+  }
+
+  async cargarClima(){
+    this.datosClima = await this._weatherApiService.getWeather().toPromise();
+    this.temperatura = this.datosClima.list[0].main.temp;
+    this.ciudad = this.datosClima.city.name;
+    this.pais = this.datosClima.city.country;
+    this.velocidadViento = this.mpsAmph(this.datosClima.list[0].wind.speed);
+    this.humedad = this.datosClima.list[0].main.humidity;
+    this.climaActual = this.datosClima.list[0].weather[0].main;
+
+  }
+
+  //conversion de velocidad metros/segundos a Millas/hora
+  mpsAmph(velocidad:number){
+    return velocidad*3600/1609.34;
   }
 
 }
