8692fe1558087f27759a7d2a96966ae918c5e6f1
Michael Merchan <michael.merchan@iroute.com.ec>
2020-08-24 18:48:48 -0500

    Agregar parametros en el enpoint

---
+++ src/app/Enumerable/enumerableUnit.ts
@@ -0,0 +1,5 @@
+export enum UnidadesCLima {
+    imperial =1 ,
+    metric = 2,
+    default = 3,
+}
--- src/app/service/weather-api.service.ts
+++ src/app/service/weather-api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { IWeatherApiResponse } from '../Interface/iweather-api-response';
+import { UnidadesCLima } from '../Enumerable/enumerableUnit';
 
 const {idCityDefault,urlapiweather,appidWeather} = environment;
 
@@ -12,8 +13,8 @@ export class WeatherApiService {
   
   constructor(private http: HttpClient) { }  
 
-  getWeather(idcity : string = idCityDefault){
-    let endPoint = `${urlapiweather}?id=${idcity}&APPID=${appidWeather}` ;
+  getWeather(idcity : string = idCityDefault,cnt:number = 1,unidades:UnidadesCLima=UnidadesCLima.metric){
+    let endPoint = `${urlapiweather}?id=${idcity}&APPID=${appidWeather}&cnt=${cnt}&units=${UnidadesCLima[unidades]}` ;
     console.log(endPoint)
     return this.http.get<IWeatherApiResponse>(endPoint);
   }
