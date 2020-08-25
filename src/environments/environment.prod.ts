d4ef952d8253ec93b20f5d9350e8117e15b64ca6
Michael Merchan <michaelmerchan@Mac-mini-de-Michael.local>
2020-08-24 23:32:07 -0500

    se agregan variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  urlapiweather:'http://api.openweathermap.org/data/2.5/forecast',
+  idCityDefault:'524901',
+  appidWeather:'e4cdac38bd3592d547523d45c59f744d',
 };
